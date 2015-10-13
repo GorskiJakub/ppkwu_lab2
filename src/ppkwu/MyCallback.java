6615a13a72f809e53bd0e03927eddb0cd8d4a1f4
Jakub G <jakubg93@o2.pl>
2015-10-13 19:05:03 +0200

    Probably working basic code

--- src/ppkwu/MyCallback.java
+++ src/ppkwu/MyCallback.java
@@ -1,23 +1,26 @@
 package ppkwu;
 
+import java.util.Scanner;
+
 public class MyCallback implements Callback{
 
 	public String callback(int result)
 	{
 		int response = 0;
 		String buf;
+		Scanner input = new Scanner(System.in);
 		if(result != 0)
 		{
 			System.out.println("Failed!!!");
 			System.out.println("Write 1 if you want to change file name:");
 			
-			buf = System.console().readLine();
+			buf = input.nextLine();
 			response = Integer.parseInt(buf);
 			if(response == 1)
 			{
 				System.out.println("Please give new file name:");
 				String buf2;
-				buf2 = System.console().readLine();
+				buf2 = input.nextLine();
 				return buf2;
 			}
 			return "";
--- src/ppkwu/main.java
+++ src/ppkwu/main.java
@@ -1,21 +1,60 @@
 package ppkwu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class main {
 
-	public static void main(String [] args)
-	{
+	private static Callback my_callback;
+
+	public static void main(String[] args) {
 		String string1;
 		String string2;
 		String fname;
-		Callback my_callback = new MyCallback();
+		my_callback = new MyCallback();
 		
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Enter string 1:");
-		string1 = System.console().readLine();
+		string1 = input.nextLine();
 		System.out.println("Enter string 2:");
-		string2 = System.console().readLine();
+		string2 = input.nextLine();
 		System.out.println("Enter fname:");
-		fname = System.console().readLine();
-		
-		try_open_and_write(string1,string2,fname,my_callback);
+		fname = input.nextLine();
+
+		try_open_and_write(string1, string2, fname, my_callback);
+	}
+
+	private static void try_open_and_write(String string1, String string2, String fname, Callback my_callback) {
+		File file = new File(fname + ".txt");
+		PrintWriter zapis;
+		Scanner input = new Scanner(System.in);
+		try {
+			zapis = new PrintWriter(fname + ".txt");
+
+			String new_fname = fname;
+			while (input.nextLine().startsWith("y")) {
+				System.out.println(string2);
+				new_fname = main.my_callback.callback(1);
+				if (new_fname.length() <= 0) {
+					break;
+				} 
+				else {
+					new_fname = new_fname.substring(0, new_fname.length() - 1);
+				}
+				if (file.exists()) {
+					zapis.println("Hello World!\n");
+					zapis.close();
+					main.my_callback.callback(0);
+					System.out.println(string1);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
